6e221b1c427669975729cf8654625cadcc3604b4
TR014777 <pauloflaflaflafla@gmail.com>
2025-05-15 14:33:24 -0300

    Alguns ajustes, anotações e agora tem a taxa da cotação

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,39 @@
 async function converter() {
+  //puxando valores dos inputs e armazenando em variáveis
   const valor = parseFloat(document.getElementById('valor').value);
-  const moedaDe = document.getElementById('moedaDe').value
+  const moedaDe = document.getElementById('moedaDe').value;
   const moedaPara = document.getElementById('moedaPara').value;
-      
+  
+  //verificando se ele é um número e se o valor é maior ou igual a 0. caso seja, não irá converter
   if (isNaN(valor) || valor <= 0) {
     document.getElementById('resultado').innerText = "Digite um valor válido.";
     return;
   }
-
+  //variáveis com links da API
   const url = `https://api.exchangerate-api.com/v4/latest/${moedaDe}`;
+  const urlPara = `https://api.exchangerate-api.com/v4/latest/${moedaPara}`
 
 try {
+  //fazendo requisições para a API e armazenando os dados necessários pra conversão
   const resposta = await fetch(url);
+  const respostaPara = await fetch(urlPara);
   const dados = await resposta.json();
+  const dadosPara = await respostaPara.json()
   const taxa = dados.rates[moedaPara];
+  const taxaPara = dadosPara.rates[moedaDe];
+  //puxando o dado da data da cotação e formatando a data
   const dataDaCotacao = dados.date
   const partesData = dataDaCotacao.split('-'); 
   const dataFormatada = `${partesData[2]}/${partesData[1]}/${partesData[0]}`;
 
+  //formatando o valor da moeda de origem
   const valorDe = new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: `${moedaDe}`,
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
   }).format(valor);
-
+  //formatando o valor da moeda de destino e convertendo-a
   const valorPara = new Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: `${moedaPara}`,
@@ -32,24 +41,28 @@ try {
     maximumFractionDigits: 2
   }).format(valor * taxa);
 
+  //puxando os textos do item selecionado do "select"
   const selectMoedaDe = document.getElementById('moedaDe');
   const textoMoedaDe = selectMoedaDe.options[selectMoedaDe.selectedIndex].text;
-
   const selectMoedaPara = document.getElementById('moedaPara');
   const textoMoedaPara = selectMoedaPara.options[selectMoedaPara.selectedIndex].text;
 
-  document.getElementById('resultado').innerText =
-    `Conversão de: ${textoMoedaDe}\n
-    Valor a converter: ${valorDe}\n
-    Para: ${textoMoedaPara}\n
-    Valor convertido: ${valorPara}\n
-    Data da cotação utilizada: ${dataFormatada}`;
-    
+  //inserindo os valores no corpo do HTMl
+  document.getElementById('conversaoDe').innerText = `Conversão de: ${textoMoedaDe}`;
+  document.getElementById('conversaoDoValorDe').innerText =  `Valor a converter: ${valorDe}`;
+  document.getElementById('conversaoPara').innerText =  `Para: ${textoMoedaPara}`;
+  document.getElementById('conversaoDoValorPara').innerText =  `Valor convertido: ${valorPara}`;
+  document.getElementById('conversaoDataDeCotacao').innerText =  `Data da cotação utilizada: ${dataFormatada}`;
+  document.getElementById('taxaDe').innerText =  `1 ${textoMoedaDe} = ${1 * taxa} ${textoMoedaPara}`;
+  document.getElementById('taxaPara').innerText =  `1 ${textoMoedaPara} = ${1 * taxaPara} ${textoMoedaDe}`;
+  
+  //caso encontre algum erro, vai dar erro ué
   } catch (erro) {
-    document.getElementById('resultado').innerText = "Erro ao buscar a cotação.";
+    document.getElementById('erro').innerText = "Erro ao buscar a cotação.";
   }
 }
 
+//faz o revesamento da moeda de origem para moeda de destino, trocando de posição
 document.getElementById('trocar').addEventListener('click', function() {
   const moedaDe = document.getElementById('moedaDe');
   const moedaPara = document.getElementById('moedaPara');
